5ac0657449a460b2c15077b0c18f6d7dba57945a
Foundation-Security-Zone <fndtnscrtzone@gmail.com>
2023-07-15 00:43:23 +0800

    Add files via upload

---
+++ IpTrack.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import time
+import json
+from urllib import request
+
+os.system("clear")
+print("\033[32;1m ")
+os.system("figlet Ip Track")
+print("VERSION 1,1")
+print("<=========================>")
+print("Welcome To Ip Track")
+print("1)Track Ip")
+print("2)Exit")
+print("<=========================>")
+select = input("Select:")
+if select == "1":
+	print("Loading...")
+	time.sleep(5)
+	url = "https://ipinfo.io/"
+ip = input("Input the IP Address : ")
+request = request.urlopen(url + ip + "/json")
+data_json = json.loads(request.read())
+
+for data in data_json:
+	info = str(data_json[data])
+	print(data + " : " + info)
+	
+	
+
+
+
+	
+	
+
+
+
+:
+	print("Loading...")
+	time.sleep(5)
+	url = "https://api.my-ip.io/ip.json"
+	request = request.urlopen(url + ip)
+	my_ip= json.loads(request.read())
+	print("succes:", my_ip["succes"])
+
+
+
+
+
+
+
